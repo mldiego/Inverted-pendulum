18f852f47358acefe67da02e37adf4cda3202b59
Diego Lopez <manzand@isis.vanderbilt.edu>
2018-11-14 16:03:10 -0600

    Modified the data collection and redo the system id in closed-loop

--- Classic_system_id.m
+++ Classic_system_id.m
@@ -1,46 +1,36 @@
 %% System Identification for the plant using classical methods
 clc;clear
 % load data
-d = load('invpend_data');
-force = d.out;
-states = d.in;
+load('invpend_data');
+
 %% Estimate the state-space model with input force and all 4 outputs
-% create iddata object 
-data = iddata(states',force',0.02);
 %define the options
-opt = n4sidOptions;
-% opt.EnforceStability = 1;
-opt.focus='simulation';
-opt.InitialState = 'estimate';
-Inv_pend = n4sid(data,4,opt);
-% estimate the output of the Vx_model
-out = lsim(Inv_pend,data.InputData);
-% Compute the mean square error for the data used to estimate the model parameters
-mse = mean((states'-out).^2)
+opt1 = n4sidOptions('N4weight','SSARX');
+opt1.EnforceStability = 1;
+opt1.focus='simulation';
+opt1.InitialState = 'estimate';
+Inv_pend = n4sid(dataf,4,opt1);
 
-%Plot the x-position graph
-t = 0:0.02:(length(force)*0.02-0.02);
-figure();
-plot(t,states(1,:));
-title('x position');
-hold on;
-plot(t,out(:,1));
-legend('Simulink', 'Estimated');
-xlabel('Time (s)');
-ylabel('Position (cm)');
+opt2 = n4sidOptions('N4weight','SSARX');
+opt2.EnforceStability = 1;
+opt2.focus='prediction';
+opt2.InitialState = 'estimate';
+Inv_pend2 = n4sid(dataf,4,opt2);
 
-%Plot the angle graphs
-figure();
-plot(t,states(3,:));
-title('Angle');
-hold on;
-plot(t,out(:,3));
-legend('Simulink', 'Estimated');
-xlabel('Time (s)');
-ylabel('Position (cm)');
 
 %% Save the model
-% convert the systems to continuous time 
+%save('Inv_pend_ss','Inv_pend');
+
+
+%% Convert the systems to continuous time 
 Inv_pend_cont = d2c(Inv_pend,'tustin');
-save('Inv_pend');
-save('Inv_pend_cont');
+Inv_pend_cont2 = d2c(Inv_pend2,'tustin');
+%save('Inv_pend_cont_ss','Inv_pend_cont');
+
+%% Test the model
+load('invpend_data_test');
+out1 = sim(Inv_pend,datatest);
+plot(out1.OutputData{4})
+hold on;
+out2 = sim(Inv_pend2,datatest);
+plot(out2.OutputData{4})
--- Collect_data_sim.m
+++ Collect_data_sim.m
@@ -3,68 +3,50 @@
 
 %% Generate training data
 clc;clear
-opt = simset('solver','ode4','SrcWorkspace','Current');
+opt = simset('solver','ode4','FixedStep',0.001);
 
-for i=1:200
+for i=1:50
     rng(1); %set the seed for reproducible results
     x0 = rand-0.5; %have a inital position between [-0.5,0.5]
     rng(1); %set the seed for reproducible results
     theta0 = rand*0.4-0.2; % have a initial angle between [-0.2,0.2]
     [T,X] = sim('ClosedLoop_Kcont',[0 15],opt); %simulate system and record data
-    
-%     x1(:,i) = X(:,1);
-    x1(:,i) = xx;
-%     x2(:,i) = X(:,2);
-    x2(:,i) = xxdot;
-%     x3(:,i) = X(:,3);
-    x3(:,i) = xtheta;
-%     x4(:,i) = X(:,4); 
-    x4(:,i) = xthetadot;
-    x5(:,i) = u;
-
+    % Create data object for each iteration
+    states = [xx xxdot xtheta xthetadot];
+    force = u;
+    data = iddata(states,force,0.005);
+    % Merge all simulations together
+    if i>1
+        dataf = merge(dataf,data);
+    else
+        dataf = merge(data);
+    end
 end
-x = reshape(x1,[],1);
-xdot = reshape(x2,[],1);
-theta = reshape(x3,[],1);
-thetadot = reshape(x4,[],1);
-force = reshape(x5,[],1);
 
-in = [x xdot theta thetadot]'; %input matrix for controller
-out = force'; %output matrix for controller
+clearvars -except dataf;
+save('invpend_data','dataf'); 
 
-clearvars -except in out;
-save('invpend_data','in','out'); 
-
-%% Generate training data
+%% Generate testing data
 clc;clear
 opt = simset('solver','ode4','SrcWorkspace','Current');
 
-for i=1:50
+for i=1:10
     rng(2); %set the seed for reproducible results
     x0 = rand-0.5; %have a inital position between [-0.5,0.5]
     rng(2); %set the seed for reproducible results
     theta0 = rand*0.4-0.2; % have a initial angle between [-0.2,0.2]
     [T,X] = sim('ClosedLoop_Kcont',[0 15],opt); %simulate system and record data
-    
-%     x1(:,i) = X(:,1);
-    x1(:,i) = xx;
-%     x2(:,i) = X(:,2);
-    x2(:,i) = xxdot;
-%     x3(:,i) = X(:,3);
-    x3(:,i) = xtheta;
-%     x4(:,i) = X(:,4); 
-    x4(:,i) = xthetadot;
-    x5(:,i) = u;
-
+    % Create data object for each iteration
+    states = [xx xxdot xtheta xthetadot];
+    force = u;
+    data = iddata(states,force,0.005);
+    % Merge all simulations together
+    if i>1
+        datatest = merge(datatest,data);
+    else
+        datatest = merge(data);
+    end
 end
-x = reshape(x1,[],1);
-xdot = reshape(x2,[],1);
-theta = reshape(x3,[],1);
-thetadot = reshape(x4,[],1);
-force = reshape(x5,[],1);
 
-in = [x xdot theta thetadot]'; %input matrix for controller
-out = force'; %output matrix for controller
-
-clearvars -except in out;
-save('invpend_data_test','in','out'); 
+clearvars -except datatest;
+save('invpend_data_test','datatest'); 
